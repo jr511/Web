b2b7e3b473b7ae79e2b305f3b7f685efbb695f9a
jr511 <jesus-r-m2011@hotmail.com>
2019-03-18 09:41:05 +0100

    delete añadido

--- notes/db/schema.rb
+++ notes/db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2019_03_16_112518) do
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.string "content"
-    t.decimal "image"
+    t.string "image"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2019_03_16_112518) do
     t.string "cover_content_type"
     t.bigint "cover_file_size"
     t.datetime "cover_updated_at"
-    t.boolean "cover", null: false
     t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
