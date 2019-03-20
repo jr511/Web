0a0606cb65c4fc671eeca75e92df0e5881e1263c
jr511 <jesus-r-m2011@hotmail.com>
2019-03-20 12:49:42 +0100

    sing up button y login en routes.rb y diseño login

--- notes/config/routes.rb
+++ notes/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'notes/index'
+  get 'users/index'
   resources :notes
   resources :users
-  root 'notes#index'
+  root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
