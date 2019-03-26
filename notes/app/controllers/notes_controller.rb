4531861c9f88065421e1259615e9353c0508d95e
jr511 <jesus-r-m2011@hotmail.com>
2019-03-26 09:38:05 +0100

    Session

--- notes/app/controllers/notes_controller.rb
+++ notes/app/controllers/notes_controller.rb
@@ -14,27 +14,36 @@ class NotesController < ApplicationController
 
   # GET /notes/new
   def new
-    @note = Note.new
+    if !session[:user]
+      redirect_to notes_path, :alert => "You have to log in to create a new note "
+    else
+      @note = Note.new
+    end
   end
 
   # GET /notes/1/edit
   def edit
+    @note = Note.find(params[:id])
+    if @note.user.name != session[:user]
+      redirect_to notes_path, :alert => "You cannot edit another user’s note!"
+    else
+      @note = Note.find(params[:id])
+    end
   end
 
   # POST /notes
   # POST /notes.json
   def create
     @note = Note.new(note_params)
-    print(note_params)
-
+    @note.user = User.find_by name: session[:user]
     respond_to do |format|
-      if @note.save
-        format.html { redirect_to @note, notice: 'Note was successfully created.' }
-        format.json { render :show, status: :created, location: @note }
-      else
-        format.html { render :new }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
-      end
+    if @note.save
+      format.html { redirect_to notes_url, notice: 'Note was successfully created.' }
+      format.json { render action: 'show', status: :created, location: @note }
+    else
+      format.html { render action: 'new' }
+      format.json { render json: @note.errors, status: :unprocessable_entity }
+    end
     end
   end
 
@@ -42,26 +51,29 @@ class NotesController < ApplicationController
   # PATCH/PUT /notes/1.json
   def update
     respond_to do |format|
-      if @note.update(note_params)
-        format.html { redirect_to @note, notice: 'Note was successfully updated.' }
-        format.json { render :show, status: :ok, location: @note }
-      else
-        format.html { render :edit }
-        format.json { render json: @note.errors, status: :unprocessable_entity }
-      end
+    if @note.update(note_params)
+      format.html { redirect_to notes_path, notice: 'Note was successfully updated.' }
+      format.json { head :no_content }
+    else
+      format.html { render action: 'edit' }
+      format.json { render json: @note.errors, status: :unprocessable_entity }
     end
+  end
   end
 
   # DELETE /notes/1
   # DELETE /notes/1.json
   def destroy
-    @note.destroy
-    respond_to do |format|
-      format.html { redirect_to notes_url, notice: 'Note was successfully destroyed.' }
+    if @note.user != session[:user]
+      redirect_to notes_path, :alert => "You cannot delete another user’s note!"
+    else
+      @note.destroy
+      respond_to do |format|
+      format.html { redirect_to notes_url }
       format.json { head :no_content }
     end
+    end
   end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_note
--- notes/config/routes.rb
+++ notes/config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
+  get 'session/new'
+  get 'session/create'
+  get 'session/destroy'
   get 'notes/index'
   get 'users/index'
+
+  get "logout" => "session#destroy", :as => "logout"
+  get "login" => "session#new", :as => "login"
+  post "login" => "session#create"
+  get "signup" => "users#new", :as => "signup"
+
   resources :notes
   resources :users
+  resources :session
   root 'users#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
